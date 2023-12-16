e3959b3a1f6e7a877b124a246545da2dd2cd63ca
prajitlal4 <prajit.lal4@gmail.com>
2023-12-16 14:35:45 +0800

    changed contact form

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -119,7 +119,6 @@ function Newsletter() {
       name="contact"
       method="POST"
       data-netlify="true"
-      action="/thank-you"
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
