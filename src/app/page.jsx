b8ca2ba7e5e0d6d8a53fafd8a242d83a7895d2ea
prajitlal4 <prajit.lal4@gmail.com>
2023-12-16 14:31:01 +0800

    changes to contact form

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -152,7 +152,7 @@ function Newsletter() {
           className="mt-2 min-w-full flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
         />
         <Button type="submit" className="mt-5  flex-none">
-          Join
+          Send
         </Button>
       </div>
     </form>
