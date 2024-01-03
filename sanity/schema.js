c4fd0755aa4361a7c24e01e43f9f7892e54a0860
prajitlal4 <prajit.lal4@gmail.com>
2024-01-03 15:54:19 +0800

    added sanity CMS to portfolio

---
+++ sanity/schema.js
@@ -0,0 +1,8 @@
+import { author } from "./schemas/author";
+import { blockContent } from "./schemas/blockContent";
+import { category } from "./schemas/category";
+import { post } from "./schemas/post";
+
+export const schema = {
+  types: [post, author, blockContent, category],
+}
